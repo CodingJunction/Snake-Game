0333b3fd88e6d738ca2ec5dac87942fc77b80c80
CodingJunction <95361536+CodingJunction@users.noreply.github.com>
2022-01-02 15:03:47 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -3,25 +3,49 @@ from turtle import Turtle
 class Score(Turtle):
     def __init__(self):
         super().__init__()
+        file=open("text.txt")
         self.score=0
+        self.highscore=int(file.read())
+        file.close()        
+        
         self.color("white")
         self.penup()
         self.goto(0,250)
         self.pendown()
-        self.write(f"Score : {self.score}",align="center",font=("Arial",24,"normal"))
+        self.write(f"Score : {self.score}  High Score : {self.highscore}",align="center",font=("Arial",24,"normal"))
         self.hideturtle()
 
     def incr(self):
         self.score+=1
+        if self.score>self.highscore:
+            self.highscore=self.score
+            with open("text.txt","w") as w:
+                w.write(str(self.highscore))
+            
         self.clear()
-        self.write(f"Score : {self.score}",align="center",font=("Arial",24,"normal"))
+        self.write(f"Score : {self.score}  High Score : {self.highscore}",align="center",font=("Arial",24,"normal"))
         self.hideturtle()
         
+
+        
     def end(self):
-        #f sn.t[0].xcor()>240 or sn.t[0].xcor()<-240 or sn.t[0].ycor()>240 or sn.t[0].ycor()<-240 :
+       
         self.penup()
         self.goto(0,0)
         self.pendown()
         self.clear()
         self.write(f"Game Over.",align="center",font=("Arial",24,"normal")) 
+
+
+    def reset(self):
+       
+        if self.score>self.highscore:
+            self.highscore=self.score
+            with open("text.txt","w") as w:
+                w.write(str(self.highscore))
+ 
+        self.score=0
+        self.clear()
+        self.write(f"Score : {self.score}  High Score : {self.highscore}",align="center",font=("Arial",24,"normal"))
+        self.hideturtle()
         
